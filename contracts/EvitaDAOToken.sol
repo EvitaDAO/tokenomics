45e5f6310ff6366e5cb2216a6310c7e680c25a73
q <quant13@gmail.com>
2022-06-15 11:22:04 +0300

    project skeleton, close #9

---
+++ contracts/EvitaDAOToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
+contract EvitaDAOToken is ERC721Upgradeable, OwnableUpgradeable {
+    string public name_;
+    string public symbol_;
+
+    // todo in 8: move to init
+    constructor() /* ERC721Upgradeable("EvitaDAO", "EVD") */ {
+        name_ = "EvitaDAO";
+        symbol_ = "EVD";
+    }
+}
