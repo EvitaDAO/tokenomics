6f795b844189f425732e6521bdbeda116d14e2a0
developerkunal <dawar.kunal703@gmail.com>
2022-06-15 19:59:41 +0530

    multi-sign added

--- contracts/EvitaToken.sol
+++ contracts/EvitaToken.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
+import "./utils/multi-sig.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 
-contract EvitaToken is ERC20BurnableUpgradeable, OwnableUpgradeable {
+contract EvitaToken is ERC20BurnableUpgradeable, OwnableUpgradeable,MultiSigWallet {
     string public name_;
     string public symbol_;
     uint256 public allSupply;
+    address public owner_first;
 
     // todo in 2: move to init
     constructor(uint256 _initialSupply) /* ERC20BurnableUpgradeable("Evita", "EVI") */ {
@@ -17,9 +19,20 @@ contract EvitaToken is ERC20BurnableUpgradeable, OwnableUpgradeable {
         symbol_ = "EVI";
         allSupply = _initialSupply;
         _mint(msg.sender, _initialSupply * 10**decimals());
+        owner_first = msg.sender;
+        //_addOwners(owner_first, 1);
+
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+    
+    function addOwners(
+        address[] memory _owners, 
+        uint _numConfirmationsRequired
+    ) public onlyOwner {
+        _addOwners(_owners, _numConfirmationsRequired);
+    }
+
 }
