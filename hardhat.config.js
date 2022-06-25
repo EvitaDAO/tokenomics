50f685972068c3ba6f5f45e089ae4b04b8fd5966
Kirill Temnov <allselead@gmail.com>
2022-06-25 15:45:59 +0300

    fix: drop extra packages #15

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require('@nomiclabs/hardhat-waffle');
 require('@openzeppelin/hardhat-upgrades');
-require('@nomiclabs/hardhat-truffle5');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
