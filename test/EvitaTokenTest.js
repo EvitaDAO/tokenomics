b803e1ef394df01a1c7b7a95733893c4aec46058
Andrey Peresleguine <peresleguine@gmail.com>
2022-06-26 17:02:20 +0200

    test: add transaction tests

--- test/EvitaTokenTest.js
+++ test/EvitaTokenTest.js
@@ -5,11 +5,12 @@ describe('EvitaToken', () => {
   let token;
   let owner;
   let user1;
+  let user2;
   const initialSupply = 111;
 
   beforeEach(async () => {
     const EvitaToken = await ethers.getContractFactory('EvitaToken');
-    [owner, user1] = await ethers.getSigners();
+    [owner, user1, user2] = await ethers.getSigners();
     token = await upgrades.deployProxy(EvitaToken, [
       'Evita',
       'EVI',
@@ -48,21 +49,43 @@ describe('EvitaToken', () => {
     });
   });
 
-  describe('Sending', () => {
-    it('Should send ether from owner to other user', async () => {
-      const transferAmount = 8;
-      const userInitialBalance = await token.balanceOf(user1.address);
-      const ownerInitialBalance = await token.balanceOf(owner.address);
-      // This one fails with Error 'ERC20: insufficient allowance'
-      await token.transferFrom(
-        owner.address,
-        user1.address,
-        ethers.utils.parseEther(`${transferAmount}`)
+  describe('Transactions', () => {
+    it('Should transfer tokens between owner and account', async function () {
+      const amount = 5;
+      const ownerBalance = await token.balanceOf(owner.address);
+
+      await token.transfer(user1.address, amount);
+
+      const newOwnerBalance = await token.balanceOf(owner.address);
+      expect(newOwnerBalance).to.equal(ownerBalance.sub(amount));
+
+      const user1Balance = await token.balanceOf(user1.address);
+      expect(user1Balance).to.equal(amount);
+    });
+
+    it('Should transfer tokens between accounts', async function () {
+      const amount = 5;
+      await token.transfer(user1.address, amount);
+      const user1Balance = await token.balanceOf(user1.address);
+      await token.connect(user1).transfer(user2.address, amount);
+
+      const newUser1Balance = await token.balanceOf(user1.address);
+      expect(newUser1Balance).to.equal(user1Balance.sub(amount));
+
+      const user2Balance = await token.balanceOf(user2.address);
+      expect(user2Balance).to.equal(amount);
+    });
+
+    it("Should fail if sender doesn't have enough tokens", async function () {
+      const initialOwnerBalance = await token.balanceOf(owner.address);
+
+      await expect(
+        token.connect(user1).transfer(owner.address, 1)
+      ).to.be.revertedWith('ERC20: transfer amount exceeds balance');
+
+      expect(await token.balanceOf(owner.address)).to.equal(
+        initialOwnerBalance
       );
-      const userFinalBalance = await token.balanceOf(user1.address);
-      const ownerFinallBalance = await token.balanceOf(owner.address);
-      expect(userFinalBalance).not.equal(userInitialBalance);
-      expect(ownerFinallBalance).not.equal(ownerInitialBalance);
     });
   });
 
